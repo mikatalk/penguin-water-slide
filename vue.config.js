2d9833a603fa4d86298bfde01788d38142ecdde1
mikatalk <mikatalk@users.noreply.github.com>
2020-02-15 21:22:13 -0800

    Small fixes

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' 
-    ? '/article-assets/create-a-liquidfun-box2d-via-web-assembly-waterslide-with-three-js/dist/'
-    // ? '//mikatalk.github.io/penguin-water-slide/'
+    // ? '/article-assets/create-a-liquidfun-box2d-via-web-assembly-waterslide-with-three-js/dist/'
+    ? '//mikatalk.github.io/penguin-water-slide/'
     : `/`,
   devServer: {
     hot: false,
